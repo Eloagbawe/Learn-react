d677c02ad0f3910f789f8b046957abacd1f85bd5
Eloagbawe <eloagbawe@gmail.com>
2022-05-28 22:56:45 +0100

    update single article page

--- simple-next-app/pages/article/[id]/index.js
+++ simple-next-app/pages/article/[id]/index.js
@@ -17,7 +17,18 @@ const article = ({article}) => {
   )
 }
 
-export const getServerSideProps = async (context) => {
+// export const getServerSideProps = async (context) => {
+//   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
+//   const article = await res.json()
+
+//   return {
+//     props: {
+//       article
+//     }
+//   }
+// }
+
+export const getStaticProps = async (context) => {
   const res = await fetch(`https://jsonplaceholder.typicode.com/posts/${context.params.id}`)
   const article = await res.json()
 
@@ -26,6 +37,19 @@ export const getServerSideProps = async (context) => {
       article
     }
   }
+}
+
+export const getStaticPaths = async () => {
+  const res = await fetch(`https://jsonplaceholder.typicode.com/posts`)
+  const articles = await res.json()
+  const ids = articles.map((article) => article.id)
+  const paths = ids.map((id) => ({params: {id: id.toString()}}))
+
+  return {
+    paths,
+    fallback: false,
+  }
+
 }
 
 export default article
